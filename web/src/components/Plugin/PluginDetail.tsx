2afce2c5dc0f76c297deeb6477a8db1e22cd8a5a
liuxiran <belovedxixi@126.com>
2021-02-02 00:14:06 +0800

    fix: 1

--- web/src/components/Plugin/PluginDetail.tsx
+++ web/src/components/Plugin/PluginDetail.tsx
@@ -33,6 +33,9 @@ import { LinkOutlined } from '@ant-design/icons';
 import Ajv from 'ajv';
 import type { DefinedError } from 'ajv';
 import addFormats from 'ajv-formats';
+import jsf from 'json-schema-faker';
+import {pathOr} from 'ramda'
+import YAML from 'yaml'
 
 import { fetchSchema } from './service';
 
@@ -98,8 +101,215 @@ const PluginDetail: React.FC<Props> = ({
     });
   }, []);
 
+  const enhance = (schema, parents = []) => (item) => {
+      console.log('----log in enhance--');
+      console.log(schema);
+      const key = item.key.value
+      console.log(key)
+      const path = [
+        ...parents.map((parent) => ['properties', parent]),
+        ['properties', key]
+      ].flat()
+      console.log(path)
+    
+      if (['title', 'description'].find((f) => path[path.length - 1] === f)) {
+        return
+      }
+    
+      const comments = [`# ${pathOr(key, [...path, 'title'], schema)} ##`, '']
+    
+      const description = pathOr('', [...path, 'description'], schema)
+      if (description) {
+        comments.push(' ' + description.split('\n').join('\n '), '')
+      }
+    
+      const defaultValue = pathOr('', [...path, 'default'], schema)
+      if (defaultValue || defaultValue === false) {
+        comments.push(' Default value: ' + defaultValue, '')
+      }
+    
+      const enums = pathOr('', [...path, 'enum'], schema)
+      if (enums && Array.isArray(enums)) {
+        comments.push(
+          ' One of:',
+          ...YAML.stringify(enums)
+            .split('\n')
+            .map((i) => ` ${i}`)
+        ) // split always returns one empty object so no need for newline
+      }
+    
+      const min = pathOr('', [...path, 'minimum'], schema)
+      if (min || min === 0) {
+        comments.push(` Minimum value: ${min}`, '')
+      }
+    
+      const max = pathOr('', [...path, 'maximum'], schema)
+      if (max || max === 0) {
+        comments.push(` Maximum value: ${max}`, '')
+      }
+    
+      const examples = pathOr('', [...path, 'examples'], schema)
+      if (examples) {
+        comments.push(
+          ' Examples:',
+          ...YAML.stringify(examples)
+            .split('\n')
+            .map((i) => ` ${i}`)
+        ) // split always returns one empty object so no need for newline
+      }
+    
+      let hasChildren
+      if (item.value.items) {
+        item.value.items.forEach((item) => {
+          if (item.key) {
+            enhance(schema, [...parents, key])(item)
+            hasChildren = true
+          }
+        })
+      }
+    
+      /* const showEnvVarBlockForObject = pathOr(
+        '',
+        [...path, 'showEnvVarBlockForObject'],
+        schema
+      )
+      if (!hasChildren || showEnvVarBlockForObject) {
+        const env = [...parents, key].map((i) => i.toUpperCase()).join('_')
+        comments.push(
+          ' Set this value using environment variables on',
+          ' - Linux/macOS:',
+          `    $ export ${env}=<value>`,
+          ' - Windows Command Line (CMD):',
+          `    > set ${env}=<value>`,
+          ''
+        )
+    
+        // Show this if the config property is an object, to call out how to specify the env var
+        if (hasChildren) {
+          comments.push(
+            ' This can be set as an environment variable by supplying it as a JSON object.',
+            ''
+          )
+        }
+      } */
+    
+      item.commentBefore = comments.join('\n')
+      item.spaceBefore = true
+    }
+
   const validateData = (pluginName: string, value: PluginComponent.Data) => {
-    return fetchSchema(pluginName, schemaType).then((schema) => {
+    return fetchSchema(pluginName, schemaType).then((test) => {
+      console.log('------schema------');
+      const schema = {
+				"$comment": "this is a mark for our injected plugin schema",
+				"anyOf": [{
+					"required": ["discovery"]
+				}, {
+					"required": ["token_endpoint"]
+				}],
+				"properties": {
+					"audience": {
+						"maxLength": 100,
+						"minLength": 1,
+						"type": "string"
+					},
+					"disable": {
+						"type": "boolean"
+					},
+					"discovery": {
+						"maxLength": 4096,
+						"minLength": 1,
+						"type": "string"
+					},
+					"grant_type": {
+						"default": "urn:ietf:params:oauth:grant-type:uma-ticket",
+						"enum": ["urn:ietf:params:oauth:grant-type:uma-ticket"],
+						"maxLength": 100,
+						"minLength": 1,
+						"type": "string"
+					},
+					"keepalive": {
+						"default": true,
+						"type": "boolean"
+					},
+					"keepalive_pool": {
+						"default": 5,
+						"minimum": 1,
+						"type": "integer"
+					},
+					"keepalive_timeout": {
+						"default": 60000,
+						"minimum": 1000,
+						"type": "integer"
+					},
+					"permissions": {
+						"items": {
+							"maxLength": 100,
+							"minLength": 1,
+							"type": "string"
+						},
+						"type": "array",
+						"uniqueItems": true
+					},
+					"policy_enforcement_mode": {
+						"default": "ENFORCING",
+						"enum": ["ENFORCING", "PERMISSIVE"],
+						"type": "string"
+					},
+					"ssl_verify": {
+						"default": true,
+						"type": "boolean"
+					},
+					"timeout": {
+						"default": 3000,
+						"minimum": 1000,
+						"type": "integer"
+					},
+					"token_endpoint": {
+						"maxLength": 4096,
+						"minLength": 1,
+						"type": "string"
+					}
+				},
+				"type": "object"
+			};
+      console.log('------jsf---------------');
+      console.log(jsf.option)
+      jsf.option({
+        alwaysFakeOptionals: true,
+        useExamplesValue: true,
+        useDefaultValue: true,
+        minItems: 1,
+        random: () => 0
+      })
+      console.log(JSON.stringify(jsf.generate(schema)));
+      console.log('----------------');
+      // console.log('----enhance schema------');
+      // console.log(enhance(schema, [])(''));
+      //console.log(YAML.parseDocument(YAML.stringify(jsf.generate(schema))))
+      console.log('----------------');
+      const values = jsf.generate(schema)
+      const doc = YAML.parseDocument(YAML.stringify(values))
+
+      const comments = [`# ${pathOr('', ['title'], schema)}`, '']
+      console.log(pathOr('',['title'], schema))
+      console.log(comments)
+      const description = pathOr('', ['description'], schema)
+      console.log(description)
+      if (description) {
+        comments.push(' ' + description)
+      }
+      console.log(comments)
+      doc.commentBefore = comments.join('\n')
+      doc.spaceAfter = false
+      doc.spaceBefore = false
+
+      doc.contents.items.forEach(enhance(schema, []))
+
+      console.log(doc)
+      console.log(doc.toString())
+      console.log('-------------------------------')
+
       return new Promise((resolve) => {
         if (schema.oneOf) {
           (schema.oneOf || []).forEach((item: any) => {
