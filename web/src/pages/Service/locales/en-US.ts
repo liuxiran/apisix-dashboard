d75b13769f0b9ba85f2ad4e5bd05f1326e3eacde
liuxiran <belovedxixi@126.com>
2021-04-27 00:20:13 +0800

    fix: bugs

--- web/src/pages/Service/locales/en-US.ts
+++ web/src/pages/Service/locales/en-US.ts
@@ -21,5 +21,6 @@ export default {
   'page.service.list': 'Service List',
   'page.service.description': 'A service consists of a combination of public plugin configuration and upstream target information in a route. Services are associated with Routes and Upstreams, and a service can correspond to a set of upstream nodes and can be bound by multiple routes.',
   'page.service.create': 'Create Service',
-  'page.service.configure': 'Configure Service'
+  'page.service.configure': 'Configure Service',
+  'page.service.fields.name.required': 'Please enter the service name',
 };
